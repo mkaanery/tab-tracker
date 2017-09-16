cabe8d153240f9a1ebfe3d6fd9d758e126689fa7
mkaanery <mahmutkaan.eryilmaz@agu.edu.tr>
2017-09-17 02:42:49 +0300

    v4 until create song button working

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -1,6 +1,17 @@
 <template>
   <v-flex xs6 offset-xs3>
     <panel title="Songs">
+      <v-btn class="orange accent-2"
+        slot="action"
+        @click="navigateTo({name: 'songs-create'})"
+        dark
+        medium
+        absolute
+        right
+        middle
+        fab>
+        <v-icon>add</v-icon>
+      </v-btn>
       List must be below:<br>
       <div v-for="song in songs" :key="song.id">
         {{song.title}}
@@ -23,6 +34,11 @@ export default {
   },
   async mounted () {
     this.songs = (await SongsService.index()).data
+  },
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
   }
 }
 </script>
